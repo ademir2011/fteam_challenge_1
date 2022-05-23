40241b67c5961d7c24850f9e6abee9498c76b06a
Ademir Bezerra <ademirbezerra2012@gmail.com>
2022-05-23 12:33:43 -0300

    associando app ao home_module

--- home_module/lib/src/home_module.dart
+++ home_module/lib/src/home_module.dart
@@ -1,9 +1,12 @@
 import 'package:dependency_module/dependency_module.dart';
+import 'package:home_module/src/presenter/pages/home_page.dart';
 
 class HomeModule extends Module {
   @override
   List<Bind> get binds => [];
 
   @override
-  List<ModularRoute> get routes => [];
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (context, args) => const HomePage()),
+      ];
 }
