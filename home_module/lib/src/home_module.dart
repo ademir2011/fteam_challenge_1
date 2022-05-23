eab5594394e872e1efe11ec708a75069b211b843
Ademir Bezerra <ademirbezerra2012@gmail.com>
2022-05-23 11:39:56 -0300

    adicionando pasta home_module

---
+++ home_module/lib/src/home_module.dart
@@ -0,0 +1,9 @@
+import 'package:dependency_module/dependency_module.dart';
+
+class HomeModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [];
+}
