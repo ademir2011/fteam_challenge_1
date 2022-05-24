ced25a2248417be21cccdbb33ffc49ff08fd6659
Ademir Bezerra <ademirbezerra2012@gmail.com>
2022-05-24 14:54:09 -0300

    envio arquivo drawer faltante do design system

--- challenge_design_system/lib/src/drawer/drawer_item_widget.dart
+++ challenge_design_system/lib/src/drawer/drawer_item_widget.dart
@@ -4,35 +4,40 @@ class DrawerItemWidget extends StatelessWidget {
   final bool isSelected;
   final String title;
   final IconData icon;
+  final void Function()? onTap;
   const DrawerItemWidget({
     Key? key,
     this.isSelected = false,
     required this.title,
     required this.icon,
+    required this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(
-          icon,
-          size: 20,
-          color: isSelected
-              ? Theme.of(context).textSelectionTheme.selectionColor
-              : Theme.of(context).colorScheme.inversePrimary,
-        ),
-        const SizedBox(width: 15),
-        Text(
-          title,
-          style: isSelected
-              ? Theme.of(context)
-                  .textTheme
-                  .labelMedium!
-                  .copyWith(color: Theme.of(context).textSelectionTheme.selectionColor)
-              : Theme.of(context).textTheme.labelMedium,
-        ),
-      ],
+    return InkWell(
+      onTap: onTap,
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            size: 20,
+            color: isSelected
+                ? Theme.of(context).textSelectionTheme.selectionColor
+                : Theme.of(context).colorScheme.inversePrimary,
+          ),
+          const SizedBox(width: 15),
+          Text(
+            title,
+            style: isSelected
+                ? Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).textSelectionTheme.selectionColor)
+                : Theme.of(context).textTheme.labelMedium,
+          ),
+        ],
+      ),
     );
   }
 }
